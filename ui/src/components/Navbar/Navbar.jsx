8d88058afe5c6c5aaceaf10f28a8daa2fa62104b
mddarr <mddarr@gmail.com>
2021-04-26 23:07:04 -0700

    navbar styling

--- ui/src/components/Navbar/Navbar.jsx
+++ ui/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 import { Alignment, Button, Navbar as Bp3Navbar } from '@blueprintjs/core'
 import c from 'classnames'
 
+import {compose} from 'redux'
+import {connect} from 'react-redux'
+
+import { withRouter } from 'react-router';
+
+
+import { SearchBox } from 'components/common/SearchBox'
+
 import './Navbar.scss'
 
 
@@ -10,9 +18,15 @@ export class Navbar extends Component {
   constructor(props) {
     super(props);
     this.navbarRef = React.createRef()
+    this.state = {
+      mobileSearchOpen: false,
+      advancedSearchOpen: false,
+    }
+  
   }
   render(){
     const {metadata} = this.props
+    const { advancedSearchOpen, mobileSearchOpen } = this.state;
     const title = "Bard Home"
     return(
       <>
@@ -26,7 +40,15 @@ export class Navbar extends Component {
             </Bp3Navbar.Group>
             
             <Bp3Navbar.Group align={Alignment.CENTER} className={c('Navbar__middle-group')}>
+              <div className="Navbar__search-container">
+                <div className="Navbar__search-container__content">
+                  <div className="Navbar__search-container__searchbar">
+                    <SearchBox 
 
+                    />
+                  </div>
+                </div>
+              </div>
             </Bp3Navbar.Group>
 
             <Bp3Navbar.Group align={Alignment.RIGHT} className={c('Navbar__right-group')}>
@@ -36,7 +58,18 @@ export class Navbar extends Component {
                     Datasets
                   </Button>
                 </Link>
+              
+                <Link to="/" >
+                  <Button icon="database" className="Navbar_collections-button bp3-minimal">
+                    Trip Reports
+                  </Button>
+                </Link>
               </>
+
+              <Bp3Navbar.Divider className={c({'mobile-hidden': mobileSearchOpen })} />
+
+
+
             </Bp3Navbar.Group>
 
 
@@ -49,4 +82,17 @@ export class Navbar extends Component {
   }
 }
 
-export default Navbar
+
+const mapStateToProps = (state, ownProps) => {
+  const {location} = ownProps;
+  return({
+    isHomepage: location.pathname === '/'
+  });
+};
+
+export default compose(
+  withRouter, 
+  connect(mapStateToProps)
+)(Navbar)
+
+// export default Navbar
---
+++ ui/src/components/common/SearchBox.jsx
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import { InputGroup } from '@blueprintjs/core';
+
+
+export class SearchBox extends PureComponent{
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.onQueryTextChange = this.onQueryTextChange.bind(this);
+    this.onSubmitSearch = this.onSubmitSearch.bind(this);
+  }
+
+  onQueryTextChange(e){
+    const queryText = e.target.value;
+    this.setState({queryText});
+  }
+
+  onSubmitSearch(event){
+    const { onSearch } = this.props;
+    const { queryText } = this.state;
+    event.preventDefault();
+    if (onSearch){
+      onSearch(queryText);
+    }
+  }
+
+  render(){
+    const { queryText } = this.state;
+    const searchPlaceholder = "placeholdery"
+    return(
+      <form onSubmit={this.onSubmitSearch} className="SearchBox">
+        <InputGroup
+          fill
+          leftIcon ="search"
+          onChange={this.onQueryTextChange}
+          placeholder={searchPlaceholder}
+          value={queryText}
+        />
+      </form>
+    )
+  }
+}
